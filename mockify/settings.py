e4a0f32f8e1f7a3271138f9842ba41ad876f6a39
Mamadou <mamadou236@gmail.com>
2020-10-17 22:54:08 -0400

    Adding django-heroku app

--- mockify/settings.py
+++ mockify/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'django_heroku',
 ]
 
 MIDDLEWARE = [
